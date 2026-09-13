05935a1fa9f2f202e1cfb043b72481be9b0123c1
piyushayu <piyush.ps1412@gmail.com>
2026-09-13 13:54:34 +0530

    update portfolio data

--- src/PortfolioData.js
+++ src/PortfolioData.js
@@ -1,13 +1,53 @@
 function getPortfolioData() {
   const portfolio = {
   "home": {
-    "name": "piyush singh",
-    "description": "jgeopge gann chdgn;egneb",
-    "github": "http://github.com"
+    "name": "piyush singh ",
+    "description": "gbrhrnherherhjetj5j"
+  },
+  "tech": {
+    "Backend": [
+      {
+        "name": "Redis",
+        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redis/redis-original.svg"
+      },
+      {
+        "name": "Spring Boot",
+        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original.svg"
+      },
+      {
+        "name": "FastAPI",
+        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg"
+      },
+      {
+        "name": "MySQL",
+        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
+      },
+      {
+        "name": "Express.js",
+        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
+      }
+    ],
+    "Frontend": [
+      {
+        "name": "Angular",
+        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg"
+      },
+      {
+        "name": "Vue.js",
+        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg"
+      },
+      {
+        "name": "React",
+        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+      },
+      {
+        "name": "Framer Motion",
+        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/framermotion/framermotion-original.svg"
+      }
+    ]
   }
 };
   return portfolio;
 }
 
 export default getPortfolioData;
-export { getPortfolioData };
