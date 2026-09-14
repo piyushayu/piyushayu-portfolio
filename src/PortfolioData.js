8ce9edccba98d2265c583b56fec9c555ed562aa3
piyushayu <piyush.ps1412@gmail.com>
2026-09-14 18:08:32 +0530

    update portfolio data

--- src/PortfolioData.js
+++ src/PortfolioData.js
@@ -1,74 +1,11 @@
-import Piyushsingh from "./assets/Piyushsingh.jpg";
-import myfreshfolio from "./assets/myfreshfolio.jpg";
+import avatar from "./assets/avatar.jpg";
 
 function getPortfolioData() {
   const portfolio = {
   "home": {
-    "name": "piyush singh ",
-    "description": "say hello on any of my socials and let's get physical "
+    "avatar": avatar
   },
-  "projects": [
-    {
-      "title": "Piyushsingh",
-      "description": "can do slave work ",
-      "image": Piyushsingh,
-      "link": "ngkejkgjke"
-    },
-    {
-      "title": "myfreshfolio",
-      "description": "virutla lnaingk assistant",
-      "link": "https:",
-      "image": "myfreshfolio"
-    }
-  ],
-  "tech": {
-    "Backend": [
-      {
-        "name": "MySQL",
-        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg"
-      },
-      {
-        "name": "FastAPI",
-        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg"
-      },
-      {
-        "name": "PostgreSQL",
-        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg"
-      },
-      {
-        "name": "Express.js",
-        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg"
-      }
-    ],
-    "Frontend": [
-      {
-        "name": "CSS3",
-        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-      },
-      {
-        "name": "TypeScript",
-        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
-      },
-      {
-        "name": "Bootstrap",
-        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
-      },
-      {
-        "name": "Redux",
-        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg"
-      },
-      {
-        "name": "Angular",
-        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg"
-      }
-    ],
-    "Technologies": [
-      {
-        "name": "Docker",
-        "image": "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg"
-      }
-    ]
-  }
+  "projects": []
 };
   return portfolio;
 }
